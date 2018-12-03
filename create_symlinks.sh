567a232d597d6673391dfb9e5d3e2c7fceedac93
Viktor Mitin <viktor.mitin.19@gmail.com>
2018-12-03 10:33:20 +0200

    added lxde panel config file

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -8,9 +8,11 @@ cd $HOME
 #backup first
 mv ~/.bashrc ~/.bashrc_bak
 mv ~/.config/openbox/lubuntu-rc.xml ~/.config/openbox/lubuntu-rc.xml_bak
+mv ~/.config/lxpanel/Lubuntu/panels/panel ~/.config/lxpanel/Lubuntu/panels/panel_bak
 
 ln -s ~/configs/.Xresources
 ln -s ~/configs/.bashrc
 ln -s ~/configs/.inputrc
 ln -s ~/configs/.vimrc
 ln -s ~/configs/lubuntu-rc.xml ~/.config/openbox/lubuntu-rc.xml
+ln -s ~/configs/panel ~/.config/lxpanel/Lubuntu/panels/panel
