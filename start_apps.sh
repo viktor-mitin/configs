15abd258aafd24d193eaebd8042ae6d8d3f83928
Viktor Mitin <viktor.mitin.19@gmail.com>
2018-12-19 14:46:25 +0200

    autostart apps scripts update

--- start_apps.sh
+++ start_apps.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sleep 1
+#touch /forcefsck
 
 ############################## run apps ###########################
 google-chrome & 
@@ -8,26 +9,27 @@ google-chrome &
 (xterm -T term1)&
 (xterm -T term2)&
 (xterm -T term3)&
-#(xterm -T term4)&
+(xterm -T term4)&
+(xterm -T term5)&
+(xterm -T term6)&
 
-#(xterm -T term5)&
-#(xterm -e 'mc')&
-
-sleep 2
+#sleep 1
 (xterm -e 'while true; do sudo htop ; done')&
 #(xterm -e 'sudo htop')$
 
 #skype&
 
 ##################### move apps to workspases ###################
-sleep 3
+sleep 2
 wmctrl -r "term1"  -t0 
 wmctrl -r "term2"  -t0 
 wmctrl -r "term3"  -t1 
-#wmctrl -r "term4"  -t1 
-#wmctrl -r "term5"  -t2 
+wmctrl -r "term4"  -t1 
+wmctrl -r "term5"  -t2 
+wmctrl -r "term6"  -t2 
 
 wmctrl -r "htop"   -t5 
+wmctrl -r "skype"  -t6 
 wmctrl -r "Google" -t8 
 
 ##################### toggle fullscreen #########################
