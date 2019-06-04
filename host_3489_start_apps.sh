8eb2bb0810a316a1610b50c6e72bd3af88935f70
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-06-04 12:08:15 +0300

    host_3489_start_apps.sh: disable sda1 mount point warning

--- host_3489_start_apps.sh
+++ host_3489_start_apps.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -x
 
-#configure 'internal' net card 
+#configure 'internal' net card
 sudo ifconfig enp2s0 192.168.1.1 up
 sudo iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
 sudo service tftpd-hpa start
 sudo service nfs-kernel-server start
 
 #NFS mount timing issue workaround, TBD
-#sleep 12 
+#sleep 12
 
 ###### ARP configuration ########
 ##disable ARP completely
@@ -15,7 +15,7 @@ sudo service nfs-kernel-server start
 # #clear ARP cache
 #sudo ip -s -s neigh flush all
 ##load static entries from /etc/ethers (format: mac ip)
-#sudo arp -f       
+#sudo arp -f
 ##arp -s 10.0.0.2 00:0c:29:c0:94:bf
 
 ##### /etc/fstab ###############
@@ -23,4 +23,4 @@ sudo service nfs-kernel-server start
 
 ##### /etc/fstab ###############
 #/dev/sda1        /n              ext4      noatime        1      1
-test -f ~/w/s/test || notify-send -t 0 "Warning: /dev/sda1 mount point is NOT available"
+#test -f ~/w/s/test || notify-send -t 0 "Warning: /dev/sda1 mount point is NOT available"
