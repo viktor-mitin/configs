7f4410593a3fb9c5b92c258436187b26589fb115
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-06-13 16:00:18 +0300

    create_symlinks.sh: fix cosmetic warnings pointed by shellcheck

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -8,20 +8,20 @@ move_to_bak ()
 	mv "$1" "$1"_bak
 }
 
-cd $HOME
+cd "$HOME"
 
 #backup first
-move_to_bak ~/.bashrc 
+move_to_bak ~/.bashrc
 move_to_bak ~/.config/openbox/lubuntu-rc.xml
 move_to_bak ~/.config/lxpanel/Lubuntu/panels/panel
 move_to_bak ~/.config/lxsession/Lubuntu/autostart
 move_to_bak ~/.config/htop/htoprc
 
-ln -s ~/configs/.Xresources
-ln -s ~/configs/.bashrc
-ln -s ~/configs/.inputrc
-ln -s ~/configs/.vimrc
-ln -s ~/configs/.gitconfig
+ln -s ~/configs/.Xresources .
+ln -s ~/configs/.bashrc .
+ln -s ~/configs/.inputrc .
+ln -s ~/configs/.vimrc .
+ln -s ~/configs/.gitconfig .
 ln -s ~/configs/lubuntu-rc.xml ~/.config/openbox/lubuntu-rc.xml
 ln -s ~/configs/panel ~/.config/lxpanel/Lubuntu/panels/panel
 ln -s ~/configs/autostart ~/.config/lxsession/Lubuntu/autostart
